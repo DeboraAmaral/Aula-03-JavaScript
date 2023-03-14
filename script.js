aa478f747efbe5844237dbb9203faebdb9808129
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-14 11:50:36 -0300

    Criando o primeiro projeto JavaScript

---
+++ script.js
@@ -0,0 +1,15 @@
+var nome = "Debora"
+var sobrenome = "Amaral"
+var idade = 17
+var sexo = "Feminino"
+var time = "São Paulo"
+var comida = "Lasanha ou Strogonoff"
+var animal = "Dog"
+
+console.log(nome)
+console.log(sobrenome)
+console.log(idade)
+console.log(sexo)
+console.log(time)
+console.log(comida)
+console.log(animal)
